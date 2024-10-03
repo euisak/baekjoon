03929f501b74004d5006bdccee5949c5ff6b62b3
euisak <166041154+euisak@users.noreply.github.com>
2024-10-04 02:19:56 +0900

    Update 검증수.py

--- Python/백준/Bronze/2475. 검증수/검증수.py
+++ Python/백준/Bronze/2475. 검증수/검증수.py
@@ -1,3 +1,9 @@
 a,b,c,d,e= map(int,input().split())
 output= (a**2+b**2+c**2+d**2+e**2)%10
 print(output)
+
+b=0
+items = list(map(int, input().split()))
+for item in items:
+    b += item**2
+print(b%10)
