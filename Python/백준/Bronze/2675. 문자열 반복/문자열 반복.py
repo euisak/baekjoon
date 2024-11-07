fc12b1bec85f1f118c9ad635ae13aea028a52423
euisak <166041154+euisak@users.noreply.github.com>
2024-11-07 12:00:04 +0900

    Update 문자열 반복.py

--- Python/백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ Python/백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -5,3 +5,20 @@ for i in range(a):
     for x in range(len(c)):
         print(c[x]*int(b), end='')
     print()
+
+
+#---------------------
+
+a= int(input())
+b= []
+
+for i in range(a):
+    c, d = input().split()
+    b.append(d)
+    c= int(c)
+
+for y in range(a): 
+    for f in range(len(b[y])):
+        for p in range(c):
+            print(b[y][f], end="")
+    print()
