d405fe7a84451d7814e868e3cd35d6fd55993e06
euisak <166041154+euisak@users.noreply.github.com>
2024-11-07 15:05:23 +0900

    Update A＋B － 4.py

--- Python/백준/Bronze/10951. A＋B － 4/A＋B － 4.py
+++ Python/백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -2,5 +2,12 @@ while True:
     try:
         a, b= map(int, input().split())
         print(a + b)
-    except: 
+    except:     # 오류 발생할 경우 except 실행
         break
+
+
+''' 
+while True:
+    a, b= map(int, input().split())   이 경우 맞지 않는 형식 입력 시 오류 발생
+    print(a + b)  
+        '''
